1f58d042071ae604c28db3911a5d6dc525abf2d9
tonyandy5630 <tonyandy456@gmail.com>
2024-11-08 16:51:36 +0700

    <feat> add demo page basic style

--- apps/docs/components/pages/LandingPage/index.tsx
+++ apps/docs/components/pages/LandingPage/index.tsx
@@ -10,8 +10,12 @@ export default function LandingPage() {
     setOpen((prev) => !prev);
   };
   return (
-    <div>
-      <Button className='text-red-300' onClick={handleToggleModal}>
+    <div className='flex justify-start items-center w-full h-screen flex-col  gap-3'>
+      <h2 className='text-5xl font-bold'>Demo UI</h2>
+      <Button
+        className='border bg-black text-white text-2xl rounded-md p-4'
+        onClick={handleToggleModal}
+      >
         Toggle Modal 2
       </Button>
       <Modal open={open} onClose={handleToggleModal}>
